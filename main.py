67bb4ddf4e4545ca0938fc27203b963decac5fcc
OrSh92 <75020539+OrSh92@users.noreply.github.com>
2021-10-04 12:14:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+from classification.classify import classify
+from racial_stats.racial_stats import get_stats
+import logging
+from registration.register import registerFile
+import argparse
+from cropping.crop import crop
+import os
+from ocr.ocr import ocr
+
+
+def main():
+    logger = logging.getLogger()
+    logger.setLevel(level=logging.DEBUG)
+    fh = logging.StreamHandler()
+    fh_formatter = logging.Formatter('%(asctime)s %(levelname)s :%(filename)s - %(message)s')
+    fh.setFormatter(fh_formatter)
+    logger.addHandler(fh)
+
+    parser = argparse.ArgumentParser(description='Extract data from historic documents')
+    parser.add_argument('-s', '--src', help="Source dir, for example: R 9361 IV_EWZ 56_K17")
+    parser.add_argument('-d', '--dst', help="Destination dir for classification, face images, "
+                                            "cropping and data extraction. Only needed if -n is not specified")
+    parser.add_argument("-n", "--no-register", help="Use this flag if the source directory is already registered", action="store_true")
+    args = parser.parse_args()
+    src_dir = args.src
+    dst_dir = args.dst
+    if not args.no_register:
+        classify(src_dir, dst_dir)
+        registerFile(dst_dir)
+    else:
+        dst_dir = src_dir
+    ocr(dst_dir)
+    get_stats(dst_dir, os.path.join(dst_dir, os.path.basename(dst_dir) + ".xlsx"))
+    crop(dst_dir)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ racial_stats/racial_stats.py
@@ -0,0 +1,70 @@
+# Imports
+from .images import BaseImage, InputImage
+from .conf.conf import BASE1_CONF, BASE2_CONF, BASE3_CONF
+import xlsxwriter
+import os
+from utils import get_registered_back_and_front
+from collections import OrderedDict
+import logging
+import re
+
+# Declaring constants
+COLUMNS = {1: '1 - Körperhöhe', 2: '2 - Wuchsform', 3: '3 - Haltung', 4: '4 - Beinlänge rel.', 5: '5 - Kopfform', 6: '6 - Hinterhaupt', 7: '7 - Gesichtsform', 8: '8 - Nasenrüeken', 9: '9 - Nasenhöhe', 10: '10 - Nasenbreite', 11: '11 - Backenknochen', 12: '12 - Augenlage', 13: '13 - Lidspalte', 14: '14 - Augenfalten-bildung', 15: '15 - Lippen', 16: '16 - Kinn', 17: '17 - Haarform', 18: '18 - Körperbehaarg.', 19: '19 - Haarfarbe', 20: '20 - Augenfarbe', 21: '21 - Hautfarbe'}
+
+type1back_path = os.path.join("registration", "bases", "type1back")
+base1 = BaseImage(os.path.join(type1back_path, "base1.jpg"), BASE1_CONF)
+base2 = BaseImage(os.path.join(type1back_path, "base2.jpg"), BASE2_CONF)
+base3 = BaseImage(os.path.join(type1back_path, "base3.jpg"), BASE3_CONF)
+bases = {
+    "Base1": base1,
+    "Base2": base2,
+    "Base3": base3
+}
+
+
+def get_stats(dir_path: str, file_path=None):
+    file_path = file_path if file_path else f'{os.path.basename(dir_path)}.xlsx'
+    results = {}
+    workbook = xlsxwriter.Workbook(file_path)
+    worksheet = workbook.add_worksheet()
+    bold = workbook.add_format({'bold': True})
+    worksheet.write(0, 0, "Dir Name", bold)
+    worksheet.write(0, 1, "File Name", bold)
+    for i in range(1, 22):
+        worksheet.write(0, i+1, COLUMNS[i], bold)
+
+    cnt = 1
+    dir_path = os.path.join(dir_path, '1', '1')
+    dir_names = sorted([int(i) for i in os.listdir(dir_path)])
+    for dir_name in dir_names:
+        dir_name = str(dir_name)
+        scan_path = os.path.join(dir_path, dir_name)
+        front_back = get_registered_back_and_front(scan_path)
+        img = front_back["back"]["registered"]
+        if not img:
+            logging.error(f"Couldn't register {front_back['back']['original']}")
+            continue
+        match_0 = re.match(r".*[\_|\-]\d+\-(Base\d+)\.jpg", img)
+        base_name = match_0.group(1)
+        base = bases.get(base_name)
+        #base = bases.get(base_0 if pic_num_0 > pic_num_1 else base_1)
+        file_path = os.path.join(scan_path, img)
+        logging.debug(f"Analyzing {img}")
+        input_image = InputImage(file_path)
+        sub_cols = input_image.get_binary_subtract_col_by_conf(base, offset_left=7, offset_right=7)
+        subtr = input_image.get_binary_subtract_by_conf(base, offset_left=-7, offset_right=-7, offset_up=0,
+                                                        offset_down=0)
+        rectangles = input_image.get_rectangles_by_conf(base, offset_left=-7, offset_right=-7, offset_up=0,
+                                                        offset_down=0)
+        verticals = InputImage.get_vertical_rectangles(rectangles)
+        results[img] = OrderedDict()
+        for i in range(1, 22):
+            col, place = InputImage.get_best_in_row_with_col(verticals, subtr, sub_cols, i, 10, 5, 10, 1)
+            row_result = float(f"{col}.{str(place).split('.')[1]}") if (col, place) != (-1, -1) else -1
+            results[img][COLUMNS[i]] = row_result
+            worksheet.write(cnt, i+1, row_result)
+        worksheet.write(cnt, 0, int(dir_name))
+        worksheet.write(cnt, 1, front_back['back']['original'])
+        cnt += 1
+    workbook.close()
+    logging.debug(f"Racial stats written to {file_path}")
